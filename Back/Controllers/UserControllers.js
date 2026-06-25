def9e04438d81c6c6b7fd4d63e7fb71829ba0bf7
leonardo mello macedo <leonardomellomacedo@gmail.com>
2026-06-25 15:27:40 -0300

    first commit

--- Back/Controllers/UserControllers.js
+++ Back/Controllers/UserControllers.js
@@ -8,10 +8,10 @@ const UserCadastro = async (req, res, next) => {
     next(error);
   }
 }
-const UserLogin = async (req, res, next) => { 
-    try {   
+const UserLogin = async (req, res, next) => {
+  try {
     const Login = await UserServices.UserLogin(req.body);
-    res.status(200).json(Login); 
+    res.status(200).json(Login);
   } catch (error) {
     next(error);
   }
@@ -32,9 +32,47 @@ const DeleteMe = async (req, res, next) => {
   }
 }
 
+// ADMIN
+const BancoDeDados = async (req, res, next) => {
+  try {
+    const user = await authService.BancoDeDados(req.body);
+
+    res.status(201).json({
+      message: "Cria um novo passo a passo no banco de dados",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const GuiaEspecifico = async (req, res, next) => {
+  try {
+    const user = await authService.GuiaEspecifico(req.body);
+
+    res.status(201).json({
+      message: "Altera as informações de um guia específico",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const DeleteGuia = async (req, res, next) => {
+  try {
+    res.status(200).json({ message: "Deleta um guia do sistema." });
+  } catch (error) {
+    next(error);
+  }
+}
+
 export default {
   UserCadastro,
   UserLogin,
   updateMe,
-  DeleteMe
+  DeleteMe,
+  BancoDeDados,
+  GuiaEspecifico,
+  DeleteGuia
 };
